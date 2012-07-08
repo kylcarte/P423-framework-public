218afd4aa9ce5be1574c9ce50acde8e3e19d3104
Kyle Carter <kylcarte@umail.iu.edu>
2012-07-08 04:53:22 -0400

    added compiled grammar files

---
+++ Framework/GenGrammars/l-01.ss
@@ -0,0 +1,31 @@
+(import (Framework match) (Framework prims))
+
+(define verify-grammar:l-01
+  (lambda (x)
+    (define Prog
+      (lambda (x)
+        (match x
+          [(begin ,(Statement -> x1) ... ,(Statement -> x2))
+           (any x2 x1)]
+          [,e (invalid-expr 'Prog e)])))
+    (define Statement
+      (lambda (x)
+        (match x
+          [(set! ,(Var -> x1) ,(Int -> x2)) (any x2 x1)]
+          [(set! ,(Var -> x1) ,(Var -> x2)) (any x2 x1)]
+          [(op-set!
+             ,(Var -> x1)
+             (,(Binop -> x2) ,(Var -> x3) ,(Int -> x4)))
+           (any x4 x3 x2 x1)]
+          [(op-set!
+             ,(Var -> x1)
+             (,(Binop -> x2) ,(Var -> x3) ,(Var -> x4)))
+           (any x4 x3 x2 x1)]
+          [,e (invalid-expr 'Statement e)])))
+    (define Var
+      (lambda (x)
+        (match x
+          [,e (guard (not [Reg e])) #f]
+          [,e (invalid-expr 'Var e)])))
+    (let ([res (Prog x)])
+      (if res (errorf 'verify-grammar:l-01 res) x))))
---
+++ FrameworkHs/GenGrammars/L01.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE StandaloneDeriving #-}
+
+module FrameworkHs.GenGrammars.L01 where
+
+import StringTable.Atom
+import Data.Int
+import FrameworkHs.Prims
+import FrameworkHs.Helpers
+
+data Prog
+  = Begin [Statement] Statement
+data Statement
+  = Set Var Int
+  | Set' Var Var
+  | OpSet Var Binop Var Int
+  | OpSet' Var Binop Var Var
+data Var
+  = Reg Reg
+
+instance PP Var where
+  pp (Reg r) = (pp r)
+instance PP Statement where
+  pp (Set v i) = (ppSexp ["set!",(pp v),(pp i)])
+  pp (Set' v v2) = (ppSexp ["set!",(pp v),(pp v2)])
+  pp (OpSet v b v2 i) = (ppSexp ["op-set!",(pp v),(ppSexp [(pp b),(pp v2),(pp i)])])
+  pp (OpSet' v b v2 v3) = (ppSexp ["op-set!",(pp v),(ppSexp [(pp b),(pp v2),(pp v3)])])
+instance PP Prog where
+  pp (Begin l s) = (ppSexp ("begin" : ((map pp l) ++ [(pp s)])))
+
+deriving instance Read Prog
+deriving instance Show Prog
+deriving instance Eq Prog
+deriving instance Ord Prog
+deriving instance Read Statement
+deriving instance Show Statement
+deriving instance Eq Statement
+deriving instance Ord Statement
+deriving instance Read Var
+deriving instance Show Var
+deriving instance Eq Var
+deriving instance Ord Var
