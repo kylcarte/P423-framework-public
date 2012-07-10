fc2a4bf4899ada384ba23f7da2ee505c29add5a2
Kyle Carter <kylcarte@umail.iu.edu>
2012-07-10 01:21:51 -0400

    Recompiled grammars

--- FrameworkHs/GenGrammars/L37ExposeFrameVar.hs
+++ FrameworkHs/GenGrammars/L37ExposeFrameVar.hs
@@ -41,29 +41,23 @@ instance PP Tail where
   pp (App t) = (ppSexp ["app",(pp t)])
   pp (Begin l t) = (ppSexp ("begin" : ((map pp l) ++ [(pp t)])))
 instance PP Prog where
-  pp (Letrec l t) = (ppSexp ["letrec",(map (\(l,t) -> (ppSexp [(pp l),(ppSexp ["lambda",(ppSexp []),(pp t)])])) l),(pp t)])
+  pp (Letrec l t) = (ppSexp ["letrec",(ppSexp (map (\(l,t) -> (ppSexp [(pp l),(ppSexp ["lambda",(ppSexp []),(pp t)])])) l)),(pp t)])
 
 deriving instance Read Prog
 deriving instance Show Prog
 deriving instance Eq Prog
-deriving instance Ord Prog
 deriving instance Read Tail
 deriving instance Show Tail
 deriving instance Eq Tail
-deriving instance Ord Tail
 deriving instance Read Effect
 deriving instance Show Effect
 deriving instance Eq Effect
-deriving instance Ord Effect
 deriving instance Read Triv
 deriving instance Show Triv
 deriving instance Eq Triv
-deriving instance Ord Triv
 deriving instance Read Var
 deriving instance Show Var
 deriving instance Eq Var
-deriving instance Ord Var
 deriving instance Read Disp
 deriving instance Show Disp
 deriving instance Eq Disp
-deriving instance Ord Disp
